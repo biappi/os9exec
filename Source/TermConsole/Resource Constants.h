04f8ebf9a555b034805e8abf59c5a3af436c7acd
bfo <>
2002-04-30 21:18:42 +0000

    Resource number definition for alertbox included

--- Source/TermConsole/Resource Constants.h
+++ Source/TermConsole/Resource Constants.h
@@ -1,1 +1,22 @@
-#define rMenuBar    128             /* application's menu bar */#define mApple                  128     /* Apple menu */#define iAbout                  1#define mFile                   129     /* File menu */#define iNew                    1#define iClose                  4#define iQuit                   12#define mEdit                   130     /* Edit menu */#define iUndo                   1#define iCut                    3#define iCopy                   4#define iPaste                  5#define iClear                  6#define iEditConfig             8#define rAboutAlert 128             /* about alert */#define rUserAlert  129             /* error user alert */#define rWindow     128             /* application's window */
+#define rMenuBar    128             /* application's menu bar */
+
+#define mApple                  128     /* Apple menu */
+#define iAbout                  1
+
+#define mFile                   129     /* File menu */
+#define iNew                    1
+#define iClose                  4
+#define iQuit                   12
+
+#define mEdit                   130     /* Edit menu */
+#define iUndo                   1
+#define iCut                    3
+#define iCopy                   4
+#define iPaste                  5
+#define iClear                  6
+#define iEditConfig             8
+
+#define rAboutAlert 128             /* about alert */
+#define rUserAlert  129             /* error user alert */
+#define rVT102Alert 130             /* error user alert */
+#define rWindow     128             /* application's window */
