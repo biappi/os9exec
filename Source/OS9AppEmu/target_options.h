c906cb0496711c974b5a037733dbf8d3a3556643
bfo <>
2002-05-10 16:14:06 +0000

    Carbon dependency included

--- Source/OS9AppEmu/target_options.h
+++ Source/OS9AppEmu/target_options.h
@@ -23,9 +23,18 @@
 #define TERMINAL_CONSOLE       // can do single char I/O
 #define USE_UAEMU              // use UAE-68k emulator
 
-#define INT_CMD     // internal commands       supported
-#define CON_SUPPORT // conole and TTYs         supported
-#define TFS_SUPPORT // transparent file system supported
-#define PIP_SUPPORT // pipes and PTYs          supported
-#define RBF_SUPPORT // RBF acesss is           supported
-#define NET_SUPPORT // TCP/IP sockets          supported
+#ifdef USE_CARBON
+   #define INT_CMD     // internal commands       supported
+   #define CON_SUPPORT // conole and TTYs         supported
+   #define TFS_SUPPORT // transparent file system supported
+   #define PIP_SUPPORT // pipes and PTYs          supported
+// #define RBF_SUPPORT // RBF acesss is           supported
+// #define NET_SUPPORT // TCP/IP sockets          supported
+#else
+   #define INT_CMD     // internal commands       supported
+   #define CON_SUPPORT // conole and TTYs         supported
+   #define TFS_SUPPORT // transparent file system supported
+   #define PIP_SUPPORT // pipes and PTYs          supported
+   #define RBF_SUPPORT // RBF acesss is           supported
+   #define NET_SUPPORT // TCP/IP sockets          supported
+#endif
