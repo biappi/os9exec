32e136faea82ba9141f87f79413c206b2ae4e858
bfo <>
2006-02-19 16:15:05 +0000

    Header changes for 2006

--- Source/OS9exec_core/consio.c
+++ Source/OS9exec_core/consio.c
@@ -23,7 +23,7 @@
 /*  Cooperative-Multiprocess OS-9 emulation   */
 /*         for Apple Macintosh, PC, Linux     */
 /*                                            */
-/* (c) 1993-2004 by Lukas Zeller, CH-Zuerich  */
+/* (c) 1993-2006 by Lukas Zeller, CH-Zuerich  */
 /*                  Beat Forster, CH-Maur     */
 /*                                            */
 /* email: luz@synthesis.ch                    */
@@ -41,6 +41,9 @@
  *    $Locker$ (who has reserved checkout)
  *  Log:
  *    $Log$
+ *    Revision 1.10  2005/06/30 11:05:48  bfo
+ *    Mach-O support
+ *
  *    Revision 1.9  2004/11/27 12:00:39  bfo
  *    _XXX_ introduced
  *
