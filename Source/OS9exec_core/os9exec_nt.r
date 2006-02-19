fbdfdd19b872ec9582bf6b82b13e7f134b8e22ce
bfo <>
2006-02-19 15:56:55 +0000

    Version changed to V3.30

--- Source/OS9exec_core/os9exec_nt.r
+++ Source/OS9exec_core/os9exec_nt.r
@@ -23,7 +23,7 @@
 /*  Cooperative-Multiprocess OS-9 emulation   */
 /*         for MacOS, Windows and Linux       */
 /*                                            */
-/* (c) 1993-2005 by Lukas Zeller, CH-Zuerich  */
+/* (c) 1993-2006 by Lukas Zeller, CH-Zuerich  */
 /*                  Beat Forster, CH-Maur     */
 /*                                            */
 /* email: luz@synthesis.ch                    */
@@ -35,10 +35,10 @@
 
 resource 'vers' (2, purgeable) {
     0x3,
-    0x28,
+    0x30,
     final,
     0x1,
     verGrSwiss,
-    "3.28",
-    "OS9exec V3.28   © by luz/bfo"
+    "3.30",
+    "OS9exec V3.30   © by luz/bfo"
 };
