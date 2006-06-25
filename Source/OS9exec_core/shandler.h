ceede5330b6f58cc1999f45629dd0e18642e54d0
bfo <>
2006-06-25 22:15:30 +0000

    Signal handler (C++) added

---
+++ Source/OS9exec_core/shandler.h
@@ -0,0 +1,68 @@
+// 
+//    OS9exec,   OS-9 emulator for Mac OS, Windows and Linux 
+//    Copyright (C) 2002 Lukas Zeller / Beat Forster
+//	  Available under http://www.synthesis.ch/os9exec
+// 
+//    This program is free software; you can redistribute it and/or 
+//    modify it under the terms of the GNU General Public License as 
+//    published by the Free Software Foundation; either version 2 of 
+//    the License, or (at your option) any later version. 
+// 
+//    This program is distributed in the hope that it will be useful, 
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+//    See the GNU General Public License for more details. 
+// 
+//    You should have received a copy of the GNU General Public License 
+//    along with this program; if not, write to the Free Software 
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+//
+
+/**********************************************/
+/*             O S 9 E x e c / NT             */
+/*  Cooperative-Multiprocess OS-9 emulation   */
+/*         for Apple Macintosh and PC         */
+/*                                            */
+/* (c) 1993-2006 by Lukas Zeller, CH-Zuerich  */
+/*                  Beat Forster, CH-Maur     */
+/*                                            */
+/* email: luz@synthesis.ch                    */
+/*        bfo@synthesis.ch                    */
+/**********************************************/
+
+/*
+ *  CVS:
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ *    $Source$
+ *    $State$
+ *    $Name$ (Tag)
+ *    $Locker$ (who has reserved checkout)
+ *  Log:
+ *    $Log$
+ *
+ */
+
+/* OS9exec signal handler */
+#ifndef __shandler_h
+#define __shandler_h
+
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+
+typedef        void (*loop_proc)( unsigned short xErr );
+void setup_exception( loop_proc lo );
+void throw_exception( unsigned short err );
+
+
+#ifdef __cplusplus
+  } // end extern "C"
+#endif
+
+
+#endif
+/* eof */
