9b5d58645f3bbccc9ee620eeaeb5cdf0cb541ae5
bfo <>
2007-02-02 12:50:09 +0000

    Files moved to directory NATIVE

---
+++ Source/NATIVE/target_options.h
@@ -0,0 +1,31 @@
+// 
+//    OS9exec,   OS-9 emulator for Mac OS, Windows and Linux 
+//    Copyright (C) 2002 Lukas Zeller / Beat Forster
+//	  Available under http://www.synthesis.ch/os9exec
+// 
+//    This program is free software; you can redistribute it and/or 
+//    modify it under the terms of the GNU General Public License as 
+//    published by the Free Software Foundation; either version 2 of 
+//    the License, or (at your option) any later version. 
+// 
+//    This program is distributed in the hope that it will be useful, 
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+//    See the GNU General Public License for more details. 
+// 
+//    You should have received a copy of the GNU General Public License 
+//    along with this program; if not, write to the Free Software 
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+//
+
+
+// Target options
+
+#define PLUGIN_DLL           // This is a Plugin DLL
+
+                             // Support for:
+                             // ============
+#define PTOC_HELLO_WORLD     // PtoC internal command "hello_world"
+#define PTOC_OPTIMIZER       //  "   speedup
+
+/* eof */
