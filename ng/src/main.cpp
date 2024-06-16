05038fd812fb82ad2ae6fc6784aeb0d5fa887a61
Antonio "Willy" Malara <antonio.malara@gmail.com>
2024-06-16 17:53:22 +0200

    disassemble helper

--- ng/src/main.cpp
+++ ng/src/main.cpp
@@ -26,14 +26,27 @@ std::string make_hex(unsigned int pc, unsigned int length)
     return hex;
 }
 
-extern "C" void cpu_instr_callback(unsigned int pc)
-{
+int disassemble_into(char *dasm, size_t len, uint32_t addr) {
     char buff[100];
+    auto instr_size = m68k_disassemble(buff, addr, AE_CPU_TYPE);
+    auto hex = make_hex(addr, instr_size);
 
-    auto instr_size = m68k_disassemble(buff, pc, AE_CPU_TYPE);
-    auto hex = make_hex(pc, instr_size);
+    snprintf(dasm, len, "%08x:     %-20s: %s", addr, hex.c_str(), buff);
+    return instr_size;
+}
 
-    printf("executing    %08x     %-20s: %s\n", pc, hex.c_str(), buff);
+extern "C" void cpu_instr_callback(unsigned int pc) {
+    char dasm[200];
+    disassemble_into(dasm, sizeof(dasm), pc);
+    printf("executing    %s\n", dasm);
+}
+
+void dump_disasm(uint32_t address, int howmany) {
+    for (int i = 0; i < howmany; i++) {
+        char dasm[200];
+        address += disassemble_into(dasm, sizeof(dasm), address);
+        printf("%s\n", dasm);
+    }
 }
 
 int main(int argc, char **argv)
@@ -84,6 +97,8 @@ int main(int argc, char **argv)
 
     space.dump();
     printf(" -\n");
+
+    dump_disasm(module_start + header.exec, 20);
 
     return 0;
 
