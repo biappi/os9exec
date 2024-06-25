9753a2cbb9d04d1d89d8750228386b42c6e0bb6b
Antonio "Willy" Malara <antonio.malara@gmail.com>
2024-06-26 00:29:39 +0200

    memory.h: inline macros

--- Source/OS9AppEmu/UAE68emulator/memory.h
+++ Source/OS9AppEmu/UAE68emulator/memory.h
@@ -7,47 +7,30 @@
   */
 
 #include "maccess.h"
-#define longget(addr)   (do_get_mem_long((uae_u32 *)addr))
-#define wordget(addr)   (do_get_mem_word((uae_u16 *)addr))
-#define byteget(addr)   (do_get_mem_byte((uae_u8  *)addr))
-#define longput(addr,l) (do_put_mem_long((uae_u32 *)addr, l))
-#define wordput(addr,w) (do_put_mem_word((uae_u16 *)addr, w))
-#define byteput(addr,b) (do_put_mem_byte((uae_u8  *)addr, b))
-
-#ifndef MD_HAVE_MEM_1_FUNCS
-
-#define longget_1 longget
-#define wordget_1 wordget
-#define byteget_1 byteget
-#define longput_1 longput
-#define wordput_1 wordput
-#define byteput_1 byteput
-
-#endif
 
 static __inline__ uae_u32 get_long(uaecptr addr)
 {
-    return longget_1(addr);
+    return do_get_mem_long((uae_u32 *)addr);
 }
 static __inline__ uae_u32 get_word(uaecptr addr)
 {
-    return wordget_1(addr);
+    return do_get_mem_word((uae_u16 *)addr);
 }
 static __inline__ uae_u32 get_byte(uaecptr addr)
 {
-    return byteget_1(addr);
+    return do_get_mem_byte((uae_u8  *)addr);
 }
 static __inline__ void put_long(uaecptr addr, uae_u32 l)
 {
-    longput_1(addr, l);
+    do_put_mem_long((uae_u32 *)addr, l);
 }
 static __inline__ void put_word(uaecptr addr, uae_u32 w)
 {
-    wordput_1(addr, w);
+    do_put_mem_word((uae_u16 *)addr, w);
 }
 static __inline__ void put_byte(uaecptr addr, uae_u32 b)
 {
-    byteput_1(addr, b);
+    do_put_mem_byte((uae_u8  *)addr, b);
 }
 
 static __inline__ uae_u8 *get_real_address(uaecptr addr)
