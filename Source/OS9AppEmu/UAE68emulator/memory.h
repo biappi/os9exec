948223c0a389a208b04fbd649de4be2d1819b70f
Antonio "Willy" Malara <antonio.malara@gmail.com>
2024-06-26 00:31:34 +0200

    maccess.h: unify with memory.h

--- Source/OS9AppEmu/UAE68emulator/memory.h
+++ Source/OS9AppEmu/UAE68emulator/memory.h
@@ -8,6 +8,48 @@
 
 #include "maccess.h"
 
+static __inline__ uae_u32 do_get_mem_long(uae_u32 *a)
+{
+    uae_u8 *b = (uae_u8 *)a;
+    
+    return (*b << 24) | (*(b+1) << 16) | (*(b+2) << 8) | (*(b+3));
+}
+
+static __inline__ uae_u16 do_get_mem_word(uae_u16 *a)
+{
+    uae_u8 *b = (uae_u8 *)a;
+    
+    return (*b << 8) | (*(b+1));
+}
+
+static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
+{
+    return *a;
+}
+
+static __inline__ void do_put_mem_long(uae_u32 *a, uae_u32 v)
+{
+    uae_u8 *b = (uae_u8 *)a;
+    
+    *b = v >> 24;
+    *(b+1) = v >> 16;    
+    *(b+2) = v >> 8;
+    *(b+3) = v;
+}
+
+static __inline__ void do_put_mem_word(uae_u16 *a, uae_u16 v)
+{
+    uae_u8 *b = (uae_u8 *)a;
+    
+    *b = v >> 8;
+    *(b+1) = (uae_u8)v;
+}
+
+static __inline__ void do_put_mem_byte(uae_u8 *a, uae_u8 v)
+{
+    *a = v;
+}
+
 static __inline__ uae_u32 get_long(uaecptr addr)
 {
     return do_get_mem_long((uae_u32 *)addr);
