eaf3930068ca1f7c2ef813008263108dfa38a8fe
Antonio "Willy" Malara <antonio.malara@gmail.com>
2024-06-26 00:12:13 +0200

    maccess.h: remove ifdeffed out code (support for bigendian emulator host)

--- Source/OS9AppEmu/UAE68emulator/maccess.h
+++ Source/OS9AppEmu/UAE68emulator/maccess.h
@@ -6,16 +6,6 @@
   * Copyright 1996 Bernd Schmidt
   */
 
-/* %%% Bfo: Linux is Intel for the moment */
-/* Note: It is not sufficient to define this in os9main_incl.h */
-/* because this SW does not import this file */
-	#define __INTEL__
-
-
-/* %%% LuZ: changed to work with x86 as well as PPC */
-#ifdef __INTEL__
-
-
 static __inline__ uae_u32 do_get_mem_long(uae_u32 *a)
 {
     uae_u8 *b = (uae_u8 *)a;
@@ -57,58 +47,4 @@ static __inline__ void do_put_mem_byte(uae_u8 *a, uae_u8 v)
 {
     *a = v;
 }
-
-// #define call_mem_get_func(func, addr) ((*func)(addr))
-// #define call_mem_put_func(func, addr, v) ((*func)(addr, v))
-
-#undef NO_INLINE_MEMORY_ACCESS
-#undef MD_HAVE_MEM_1_FUNCS
-
-
-/* %%% LuZ */
-#endif // __INTEL__
-
-
-/* %%% LuZ: changed to work with x86 as well as PPC */
-#if (defined macintosh || defined __MACH__) && !defined __INTEL__
-
-static __inline__ uae_u32 do_get_mem_long(uae_u32 *a)
-{
-    return *a;
-}
-
-static __inline__ uae_u16 do_get_mem_word(uae_u16 *a)
-{
-    return *a;
-}
-
-static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
-{
-    return *a;
-}
-
-static __inline__ void do_put_mem_long(uae_u32 *a, uae_u32 v)
-{
-    *a = v;
-}
-
-static __inline__ void do_put_mem_word(uae_u16 *a, uae_u16 v)
-{
-    *a = v;
-}
-
-static __inline__ void do_put_mem_byte(uae_u8 *a, uae_u8 v)
-{
-    *a = v;
-}
-
-//#define call_mem_get_func(func, addr) ((*func)(addr))
-//#define call_mem_put_func(func, addr, v) ((*func)(addr, v))
-
-#undef NO_INLINE_MEMORY_ACCESS
-#undef MD_HAVE_MEM_1_FUNCS
-
-// %%% LuZ
-#endif // macintosh
-
 
