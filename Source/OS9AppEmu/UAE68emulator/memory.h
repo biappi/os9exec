aef064d830fbf768a32d6bc07d558d2137c416de
Enrico Gueli <enrico.gueli@gmail.com>
2024-07-07 14:56:54 +0200

    remove __inline__

--- Source/OS9AppEmu/UAE68emulator/memory.h
+++ Source/OS9AppEmu/UAE68emulator/memory.h
@@ -6,26 +6,26 @@
   * Copyright 1995 Bernd Schmidt
   */
 
-static __inline__ uae_u32 get_long(uaecptr addr)
+static uae_u32 get_long(uaecptr addr)
 {
     uae_u8 *a = (uae_u8 *)addr;
     uae_u8 *b = (uae_u8 *)a;
 
     return (*b << 24) | (*(b+1) << 16) | (*(b+2) << 8) | (*(b+3));
 }
-static __inline__ uae_u32 get_word(uaecptr addr)
+static uae_u32 get_word(uaecptr addr)
 {
     uae_u8 *a = (uae_u8 *)addr;
     uae_u8 *b = (uae_u8 *)a;
 
     return (*b << 8) | (*(b+1));
 }
-static __inline__ uae_u32 get_byte(uaecptr addr)
+static uae_u32 get_byte(uaecptr addr)
 {
     uae_u8 *a = (uae_u8 *)addr;
     return *a;
 }
-static __inline__ void put_long(uaecptr addr, uae_u32 v)
+static void put_long(uaecptr addr, uae_u32 v)
 {
     uae_u8 *a = (uae_u8 *)addr;
     uae_u8 *b = (uae_u8 *)a;
@@ -35,7 +35,7 @@ static __inline__ void put_long(uaecptr addr, uae_u32 v)
     *(b+2) = v >> 8;
     *(b+3) = v;
 }
-static __inline__ void put_word(uaecptr addr, uae_u32 v)
+static void put_word(uaecptr addr, uae_u32 v)
 {
     uae_u8 *a = (uae_u8 *)addr;
     uae_u8 *b = (uae_u8 *)a;
@@ -43,7 +43,7 @@ static __inline__ void put_word(uaecptr addr, uae_u32 v)
     *b = v >> 8;
     *(b+1) = (uae_u8)v;
 }
-static __inline__ void put_byte(uaecptr addr, uae_u32 v)
+static void put_byte(uaecptr addr, uae_u32 v)
 {
     uae_u8 *a = (uae_u8 *)addr;
     *a = v;
